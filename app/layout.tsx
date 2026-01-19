d3ebb1ba196fd9b76c58514133ca810dea3d2f4a
Debmalya Sen <debmalyasen37@gmail.com>
2026-01-19 19:11:23 +0530

    feat: Update Google Tag Manager integration to Google Analytics 4 and remove unnecessary noscript tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,25 +33,25 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         {/* ===============================
-            Google Tag Manager (HEAD)
-           =============================== */}
+            Google Analytics 4 (gtag.js)
+            =============================== */}
         <Script
-          id="gtm-head"
-          strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-59MFV9VF');
-            `,
-          }}
+          src="https://www.googletagmanager.com/gtag/js?id=G-Q3EKPEFH37"
+          strategy="afterInteractive"
         />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Q3EKPEFH37');
+          `}
+        </Script>
 
         {/* ===============================
             Meta (Facebook) Pixel
-           =============================== */}
+            =============================== */}
         <Script
           id="facebook-pixel"
           strategy="afterInteractive"
@@ -77,21 +77,11 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         suppressHydrationWarning
       >
-        {/* ===============================
-            Google Tag Manager (NOSCRIPT)
-           =============================== */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-59MFV9VF"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+        {/* Note: The GTM <noscript> tag has been removed as gtag.js does not require it */}
 
         {/* ===============================
             Meta Pixel (NOSCRIPT)
-           =============================== */}
+            =============================== */}
         <noscript>
           <img
             height="1"
@@ -104,7 +94,7 @@ export default function RootLayout({
 
         <ReduxProvider>
           <Suspense fallback={null}>
-            {/* SPA route tracking */}
+            {/* SPA route tracking - Verify this component supports GA4/gtag logic */}
             <GTMPageView />
 
             {/* App content */}
